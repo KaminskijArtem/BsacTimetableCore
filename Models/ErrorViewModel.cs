ca17bfacd995b047b936144baf3c0d3b4a4d35d3
Unknown <artemkaminskij2016@gmail.com>
2017-09-05 22:34:08 +0300

    Standart Core project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BsacTimetableCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
