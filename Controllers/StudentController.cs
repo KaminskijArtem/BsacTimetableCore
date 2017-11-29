b1f26537bf080ef5880daf5c650684d6ce294f2c
Unknown <artemkaminskij2016@gmail.com>
2017-11-29 22:25:37 +0300

    Oleg fix

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -42,14 +42,12 @@ namespace BsacTimetableCore.Controllers
                                      where p.IdGroup == idgroup
                                      select p.NameGroup).First();
 
-            var sgId = subgroup + 2;//IDK why, but it shoud be before linq
-
             var records = (from r in _context.Record
                            join l in _context.Lecturer on r.IdLecturer equals l.IdLecturer
                            join s in _context.Subject on r.IdSubject equals s.IdSubject
                            join c in _context.Classroom on r.IdClassroom equals c.IdClassroom
                            where (r.IdGroup == idgroup) &&
-                           new[] { sgId, 1, 2 }.Contains(r.IdSubjectFor)
+                           new[] { subgroup, 3 }.Contains(r.IdSubjectFor)
 
                            //    && (r.DateTo >= DateTime.Today && r.DateFrom <= DateTime.Today)
                            orderby r.WeekDay, r.SubjOrdinalNumber, r.WeekNumber
